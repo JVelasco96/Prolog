a3b3c9d5ab9dc290604f52478fa6e8cfb365a81a
JVelasco96 <43820401+JVelasco96@users.noreply.github.com>
2019-11-07 16:17:44 -0700

    Add files via upload
    Primer commit

---
+++ cargas-materias.pl
@@ -0,0 +1,8 @@
+materias([
+    [[calculo1,5],     [calculo2,5],     [calculo3,5],          [calculo4,5]],    [graficacion,4],[[lenguajesAutomatas1,5],[lenguajesAutomatas,5]],
+    [[programacion1,5],[programacion2,5],[programacion3,5],     [programacion4,5],[metodosNum,4]],[[sistemasOp1,4],        [sistemasOp2,4]],
+    [etica,4],         [contabilidad,4], [culturaEmpresarial,4],[investigacion,4],[[redes1,4],     [redes2,5],             [redes3,5]],
+    [matematicas,5],   [quimica,4],      [io,5],                [[bd1,5],         [bd2,5],         [bd3,5]],
+    [admin,4],         [algebra,5],      [desarrollo,5],        [simulacion,5],   [[ingSw1,4],     [ingSw2,5],             [ingSw3,6]]
+    ]).
+
